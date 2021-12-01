b36c94276b186bae0f010b6d802b4d3712275345
PrashantGupta06 <94178060+PrashantGupta06@users.noreply.github.com>
2021-12-01 16:44:38 +0530

    Add files via upload

---
+++ Prashant Gupta/homework.py
@@ -0,0 +1,13 @@
+list = [18,20,8,16,4,10,12,3]
+list.sort()
+num = list[4]
+count = 0
+
+while num!=0:
+    if num%2==0:
+        num/=2
+        count+=1
+    elif num%2!=0:
+        num-=1    
+        count+=1
+print(count)
